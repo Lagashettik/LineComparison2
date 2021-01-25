9ecae66c851d8371d977d8f239616b8f039f6ec0
Lagashettik <Lagashettik@gmail.com>
2021-01-25 19:08:55 +0530

    Welcome Message

---
+++ LineComparison2.java
@@ -0,0 +1,12 @@
+
+public class LineComparison2 {
+
+	LineComparison2(){
+	System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+	public static void main(String args[]){
+	LineComparison2 Line= new LineComparison2();
+
+	}
+}
