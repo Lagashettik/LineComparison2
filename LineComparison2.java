161e343a33f7c35a2635917ae7c9d7ee2eed6792
Lagashettik <Lagashettik@gmail.com>
2021-01-26 12:45:04 +0530

    Used: Java OOPs Concepts and Methods

--- LineComparison2.java
+++ LineComparison2.java
@@ -5,7 +5,7 @@ public class LineComparison2 {
 	int Coordinate_y1;
 	int Coordinate_x2;
 	int Coordinate_y2;
-
+	String S1;
 	LineComparison2(){}
 
 	LineComparison2(String s){
@@ -13,32 +13,44 @@ public class LineComparison2 {
 	}
 
 	double LineCalc() {
-	Scanner In = new Scanner(System.in);
-	Coordinate_x1 = In.nextInt();
-	Coordinate_y1 = In.nextInt();
-	Coordinate_x2 = In.nextInt();
-	Coordinate_y2 = In.nextInt();
+		Scanner In = new Scanner(System.in);
+		Coordinate_x1 = In.nextInt();
+		Coordinate_y1 = In.nextInt();
+		Coordinate_x2 = In.nextInt();
+		Coordinate_y2 = In.nextInt();
 
-	double Length = Math.sqrt(Math.pow((Coordinate_x2-Coordinate_x1),2) + Math.pow((Coordinate_y2-Coordinate_y1),2));
-	System.out.println("Length of line: "+Length);
-	return Length;
+		double Length = Math.sqrt(Math.pow((Coordinate_x2-Coordinate_x1),2) + Math.pow((Coordinate_y2-Coordinate_y1),2));
+		System.out.println("Length of line: "+Length);
+		return Length;
+		}
+
+	void ConvertToString(LineComparison2 Line2){
+		S1= String.valueOf(this.LineCalc());
+		Line2.S1= String.valueOf(Line2.LineCalc());
+		}
+
+	void CompareTo(LineComparison2 Line2){
+	double var =S1.compareTo(Line2.S1);
+
+	if(var > 0){
+		System.out.println("Length of Line1 is Greater");
+		}
+	if(var < 0){
+		System.out.println("Length of Line2 is Greater");
+		}
 	}
 
 	void Equals(LineComparison2 Line2){
-	String s1= String.valueOf(this.LineCalc());
-	String s2= String.valueOf(Line2.LineCalc());
+	this.ConvertToString(Line2);
+	boolean var =S1.equals(Line2.S1);
 
-	double var =s1.compareTo(s2);
-
-	if(var == 0 ){
-		System.out.println("Both Lines Length are equal");
+	if(var == true ){
+		System.out.println("Both Lines lengths are equal");
 		}
-	else if(var > 0){
-		System.out.println("Length of Line1 is Greater");
+	else {
+		System.out.println("Both Lines lengths are not equal");
 		}
-	else{
-		System.out.println("Length of Line2 is Greater");
-		}
+		this.CompareTo(Line2);
 	}
 
 	public static void main(String args[]){
